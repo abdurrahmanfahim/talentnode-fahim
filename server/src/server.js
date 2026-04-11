86c616e84e972972c4744a152fc65b73e4460015
Abdur Rahman Fahim <127137394+abdurrahmanfahim@users.noreply.github.com>
2026-04-11 20:55:28 +0600

    chore: initialize server with express and dependencies

---
+++ server/src/server.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const multer = require('multer')
+const cookieParser = require('cookie-parser')
+const { PORT, ORIGIN } = process.env
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(multer().none())
+app.use(cors({
+    origin: ORIGIN || "http://localhost:5173",
+    credentials: true
+}))
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`)
+})
